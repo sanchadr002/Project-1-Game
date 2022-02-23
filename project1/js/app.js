3960379f56d63c88d5743a67b3b674a0961dfd58
sanchadr002 <sanchadr002@gmail.com>
2022-02-23 12:39:15 -0800

    add functions to spawn projectiles at random points on y axis

--- project1/js/app.js
+++ project1/js/app.js
@@ -6,6 +6,7 @@ const patienceDisplay = document.getElementById('patience-display')
 const ctx = game.getContext('2d')
 
 // create homeless & projectiles using a class constructor
+// used the class constructor code from canvas crawler as a template for this
 class MovingThings {
     constructor(x, y, color, height, width){
         this.x = x,
@@ -22,12 +23,38 @@ class MovingThings {
 }
 
 let homeless = new MovingThings(10, 10, 'goldenrod', 10, 10)
-let food = new MovingThings(290, Math.floor(Math.random() * 145), 'brown', 5, 5)
-let hairbrush = new MovingThings (100, Math.floor(Math.random() * 130), 'greenyellow', 20, 30)
-let vacuum = new MovingThings(100, Math.floor(Math.random() * 120), 'grey', 30, 50)
-vacuum.render()
-hairbrush.render()
 
+// functions to create different elements at random points on the y axis
+const randomFood = () => {
+    // use class to create each object
+    let food = new MovingThings (100, Math.floor(Math.random() * 145), 'brown', 5, 5)
+    // render it to show it on the screen
+    food.render()
+    // (homeless twirls when she's very excited)
+    console.log('homeless twirls!')
+    // return the object so it can be manipulated outside of the function
+    return food
+}
+
+const randomHairbrush = () => {
+    let hairbrush = new MovingThings (100, Math.floor(Math.random() * 130), 'greenyellow', 20, 30)
+    hairbrush.render()
+    console.log('pew pew')
+    return hairbrush
+}
+
+const randomVacuum = () => {
+    let vacuum = new MovingThings(100, Math.floor(Math.random() * 120), 'grey', 30, 50)
+    vacuum.render()
+    console.log('boom')
+    return vacuum
+}
+
+// const spawnProjectiles = () => {
+//     setInterval(randomFood, 3000)
+//     setInterval(randomHairbrush, 5000)
+//     setInterval(randomVacuum, 7000)
+// }
 
 // create unit collision/hit detection
 
@@ -44,9 +71,16 @@ pointsDisplay.innerText = 0
 
 // function for randomly spawning projectiles
 // setInterval inside or outside??
-const spawnProjectiles = () => {
-    setInterval()
-}
+// BUG - PROJECTILES SPAWN IN PLACE EVERY 5 SECONDS INSTEAD OF SPAWNING AT A RANDOM Y COORDINATE
+//       IT IS ONLY RENDERING, NOT ACTUALLY CREATING A NEW PROJECTILE
+//       WRITE A FUNCTION THAT ACTUALLY CREATES A NEW PROJECTILE AND RENDERS IT
+// const spawnProjectiles = () => {
+//     hairbrush.render()
+//     vacuum.render()
+//     console.log('pew pew')
+// }
+
+// setInterval(spawnProjectiles, 5000)
 
 // function for increasing score on food catch
 // on food hit detection, increase score by 10

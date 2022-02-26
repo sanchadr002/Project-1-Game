709fad1aa18e1bc3f3ffc006ad198492b9601278
sanchadr002 <sanchadr002@gmail.com>
2022-02-26 15:44:47 -0800

    connect points and patience displays to JS code to change content on hit detection

--- project1/js/app.js
+++ project1/js/app.js
@@ -23,113 +23,92 @@ class MovingThings {
         // function that we can call to have that individual piece move
         // this function will have to erase and replace each piece to simulate movement
         this.scrollLeft = function () {
-            // setInterval() does exactly what we need for this
-            // setInterval(() => {
-                // eliminate each piece before the stats are changed
-                
-                ctx.clearRect(this.x, this.y, this.width, this.height)
-                if (!this.alive || this.x === 0){
-                    return
+                 // function to simulate the projectile being removed on a hit
+                 if (!this.alive){
+                    this.x = 500
+                    this.y = 500
                 }
+                // eliminate each piece before the stats are changed
+                ctx.clearRect(this.x, this.y, this.width, this.height)
                 // change each relevant stat by a fixed amount
-                this.x -= 2
-                this.width - 2
+                this.x -= 5
+                this.width - 5
                 this.render()
-            // }, 60)
-            // if (!this.alive){
-            //     clearInterval()
-            // }
         }
     }
-}
+}food
 
 let homeless = new MovingThings(10, 10, 'goldenrod', 10, 10)
-let food = new MovingThings (200, Math.floor(Math.random() * 145), 'brown', 5, 5)
-let hairbrush = new MovingThings (200, Math.floor(Math.random() * 130), 'greenyellow', 20, 30)
-let vacuum = new MovingThings(200, Math.floor(Math.random() * 110), 'grey', 30, 50)
-
-let projectilesArr = [];
-
-
+let food = new MovingThings (275, Math.floor(Math.random() * 145), 'brown', 5, 5)
+let hairbrush = new MovingThings (275, Math.floor(Math.random() * 130), 'greenyellow', 20, 30)
+let vacuum = new MovingThings(275, Math.floor(Math.random() * 110), 'grey', 30, 50)
 
 // create unit collision/hit detection
 // used canvas crawler as a template
 const detectHit = () => {
-    if (homeless.x < vacuum.x + vacuum.width
-        && homeless.x + homeless.width > vacuum.x
-        && homeless.y < vacuum.y + vacuum.height
-        && homeless.y + homeless.height > vacuum.y){
-            vacuum.alive = false
-            ctx.clearRect(vacuum.x, vacuum.y, vacuum.width, vacuum.height)
-            
-            console.log('vacuum hit')
+    const pointsUp = () => {
+        pointsDisplay.innerHTML++
+    }
+    const patienceDown = () => {
+        patienceDisplay.innerHTML--
+    }
+    if (homeless.x < food.x + food.width
+        && homeless.x + homeless.width > food.x
+        && homeless.y < food.y + food.height
+        && homeless.y + homeless.height > food.y){
+            food.alive = false
+            pointsUp()
+            ctx.clearRect(food.x, food.y, food.width, food.height)
+            console.log('food hit')
     } else if (homeless.x < hairbrush.x + hairbrush.width
         && homeless.x + homeless.width > hairbrush.x
         && homeless.y < hairbrush.y + hairbrush.height
         && homeless.y + homeless.height > hairbrush.y){
             hairbrush.alive = false
+            patienceDown()
             ctx.clearRect(hairbrush.x, hairbrush.y, hairbrush.width, hairbrush.height)
-            
             console.log('hairbrush hit')
-    } else if (homeless.x < food.x + food.width
-        && homeless.x + homeless.width > food.x
-        && homeless.y < food.y + food.height
-        && homeless.y + homeless.height > food.y){
-            food.alive = false
-            ctx.clearRect(food.x, food.y, food.width, food.height)
-            
-            console.log('food hit')
+    } else if (homeless.x < vacuum.x + vacuum.width
+        && homeless.x + homeless.width > vacuum.x
+        && homeless.y < vacuum.y + vacuum.height
+        && homeless.y + homeless.height > vacuum.y){
+            vacuum.alive = false
+            patienceDown()
+            ctx.clearRect(vacuum.x, vacuum.y, vacuum.width, vacuum.height)
+            console.log('vacuum hit')
     }
 }
 
 const gameLoop = () => {
-    
     ctx.clearRect(0, 0, game.width, game.height)
-    
     vacuum.scrollLeft()
     hairbrush.scrollLeft()
     food.scrollLeft()
-    
     detectHit()
     homeless.render()
-    if (!food.alive){
-        food.x = 500
-        food.y = 500
-    } else if (!hairbrush.alive){
-        hairbrush.x = 500
-        hairbrush.y = 500
-    } else if (!vacuum.alive){
-        vacuum.x = 500
-        vacuum.y = 500
-    }
 } 
 // functions to create different elements at random points on the y axis
 const randomFood = () => {
     // use class to create each object
-    food = new MovingThings (200, Math.floor(Math.random() * 145), 'brown', 5, 5)
-    projectilesArr.push(food)
-    // render it to show it on the screen
-    // make it scroll left
+    food = new MovingThings (250, Math.floor(Math.random() * 145), 'brown', 5, 5)
     // (homeless twirls when she's very excited)
     console.log('homeless twirls!')
 }
 
 const randomHairbrush = () => {
-    hairbrush = new MovingThings (200, Math.floor(Math.random() * 130), 'greenyellow', 20, 30)
-    projectilesArr.push(hairbrush)
+    hairbrush = new MovingThings (250, Math.floor(Math.random() * 130), 'greenyellow', 20, 30)
     console.log('pew pew')
 }
 
 const randomVacuum = () => {
-    vacuum = new MovingThings(200, Math.floor(Math.random() * 110), 'grey', 30, 50)
-    projectilesArr.push(vacuum)
+    vacuum = new MovingThings(250, Math.floor(Math.random() * 110), 'grey', 30, 50)
     console.log('boom')
 }
 
 const spawnProjectiles = () => {
-    setInterval(randomVacuum, 10000)
-    setInterval(randomHairbrush, 7000)
-    setInterval(randomFood, 5000)
+    setInterval(randomVacuum, 9000)
+    setInterval(randomHairbrush, 6000)
+    setInterval(randomFood, 4000)
     
 }
 
@@ -155,7 +134,6 @@ const movementHandler = (e) => {
 
 // grab score display (100 points to win)
 // its content start at 0 and increase by however much with each food hit
-pointsDisplay.innerText = 0
 
 // create homeless's desired food type display
 
@@ -167,10 +145,10 @@ pointsDisplay.innerText = 0
 
 
 // function for increasing score on food catch
-// on food hit detection, increase score by 10
+// on food hit detection, increase score by 1
 // MOVE this function to inside hit detection section
 const pointsUp = () => {
-    pointsDisplay.innerText += 10
+    pointsDisplay.innerHTML++
 }
 
 // function for decreasing patience on hairbrush/vacuum hit
@@ -181,6 +159,8 @@ const pointsUp = () => {
 // function for win detection
 
 document.addEventListener('DOMContentLoaded', () => {
+    pointsDisplay.innerHTML = 0
+    patienceDisplay.innerHTML = 5
     document.addEventListener('keydown', movementHandler)
     spawnProjectiles()
     setInterval(gameLoop, 30)
